ed15aba0e90afdca22b9588f0a8bb66ce3464d22
michellejanosi <michelle.janosi@gmail.com>
2022-08-13 16:42:18 -0400

    Refactor renderCharacter function ad add dice roll function

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const hero = {
   name: 'Wizard',
   avatar: 'images/wizard.png',
   health: 60,
-  diceRoll: 6,
+  diceRoll: [3, 1, 4],
+  diceCount: 3,
 };
 
 const monster = {
@@ -11,18 +12,34 @@ const monster = {
   name: 'Orc',
   avatar: 'images/orc.png',
   health: 10,
-  diceRoll: 4,
+  diceRoll: [2],
+  diceCount: 1,
 };
 
+function getDiceRollArray(diceCount) {
+  const randNums = [];
+
+  for (let i = 0; i < diceCount; i++) {
+    const num = Math.floor(Math.random() * 6) + 1;
+    randNums.push(num);
+  }
+
+  return randNums;
+}
+
 function renderCharacter(data) {
-  const { elementId, name, avatar, health, diceRoll } = data;
-  document.getElementById(elementId).innerHTML = 
-  `<div class="character-card">
+  const { elementId, name, avatar, health, diceRoll, diceCount } = data;
+
+  const diceHtml = diceRoll.map(die => {
+    return `<div class="dice">${die}</div>`;
+  }).join('');
+
+  document.getElementById(elementId).innerHTML = `<div class="character-card">
       <h4 class="name"> ${name} </h4>
       <img class="avatar" src="${avatar}" />
       <div class="health">health: <b> ${health} </b></div>
       <div class="dice-container">
-          <div class="dice"> ${diceRoll} </div>
+          ${diceHtml} 
       </div>
    </div>`;
 }
