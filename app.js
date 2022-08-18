e4b641a45702f045438482803d548f1566bdc0cb
michellejanosi <michelle.janosi@gmail.com>
2022-08-18 05:50:40 -0400

    Move character data to its own file

--- app.js
+++ app.js
@@ -1,19 +1,4 @@
-const characterData = {
-  hero: {
-    elementId: 'hero',
-    name: 'Wizard',
-    avatar: 'images/wizard.png',
-    health: 60,
-    diceCount: 3,
-  },
-  monster: {
-    elementId: 'monster',
-    name: 'Orc',
-    avatar: 'images/orc.png',
-    health: 10,
-    diceCount: 1,
-  },
-};
+import characterData from './data.js';
 
 function Character(data) {
   Object.assign(this, data);
