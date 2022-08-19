a6ee6f87d113659a2c4b04b1ab66957e5b65619a
michellejanosi <michelle.janosi@gmail.com>
2022-08-19 07:45:27 -0400

    Clean up character data

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ import characterData from './data.js';
 import Character from './Character.js';
 
 function render() {
-  document.getElementById(wizard.elementId).innerHTML = wizard.getCharacterHtml();
-  document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml();
+  document.getElementById('hero').innerHTML = wizard.getCharacterHtml();
+  document.getElementById('monster').innerHTML = orc.getCharacterHtml();
 }
 
 const wizard = new Character(characterData.hero);
