441231ad15d4fec7c205d8611ded8b1c58ec2fb1
michellejanosi <michelle.janosi@gmail.com>
2022-09-04 08:10:06 -0400

    Disable attack button when pause between deaths and end game

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import characterData from './data.js';
 import Character from './Character.js';
 
 let monsters = ['orc', 'demon', 'goblin'];
+let isWaiting = false;
 
 function getNewMonster() {
   const nextMonsterData = characterData[monsters.shift()];
@@ -9,29 +10,32 @@ function getNewMonster() {
 }
 
 function attack() {
-  wizard.getDiceHtml();
-  monster.getDiceHtml();
-  wizard.takeDamage(monster.currentDiceScore);
-  monster.takeDamage(wizard.currentDiceScore);
-  render();
+  if (!isWaiting) {
+    wizard.getDiceHtml();
+    monster.getDiceHtml();
+    wizard.takeDamage(monster.currentDiceScore);
+    monster.takeDamage(wizard.currentDiceScore);
+    render();
 
-  if (wizard.dead) {
-    gameOver();
-  } else if (monster.dead) {
+    if (wizard.dead) {
+      gameOver();
+    } else if (monster.dead) {
+      isWaiting = true;
       if (monsters.length > 0) {
         setTimeout(() => {
           monster = getNewMonster();
           render();
+          isWaiting = false;
         }, 1500);
       } else {
         gameOver();
+      }
     }
   }
-
-
 }
 
 function gameOver() {
+  isWaiting = true;
   const endMessage = wizard.health === 0 && monster.health === 0 ? 'No victors - all creatures are dead'
     : wizard.health > 0 ? 'The Wizard Wins'
     : 'The monster is Victorious';
