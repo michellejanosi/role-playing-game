eca7a477e113f05b6b5311a4ddcdbcc7f669b46c
michellejanosi <michelle.janosi@gmail.com>
2022-09-04 07:05:37 -0400

    Fix orc data

--- data.js
+++ data.js
@@ -1,17 +1,31 @@
 const characterData = {
   hero: {
-    name: 'Wizard',
-    avatar: 'images/wizard.png',
-    health: 40,
+    name: "Wizard",
+    avatar: "images/wizard.png",
+    health: 60,
     diceCount: 3,
     currentDiceScore: [],
   },
-  monster: {
-    name: 'Orc',
-    avatar: 'images/orc.png',
-    health: 60,
+  orc: {
+    name: "Orc",
+    avatar: "images/orc.png",
+    health: 30,
     diceCount: 1,
-    currentDiceScore: []
+    currentDiceScore: [],
+  },
+  demon: {
+    name: "Demon",
+    avatar: "images/demon.png",
+    health: 25,
+    diceCount: 2,
+    currentDiceScore: [],
+  },
+  goblin: {
+    name: "Goblin",
+    avatar: "images/goblin.png",
+    health: 20,
+    diceCount: 3,
+    currentDiceScore: [],
   },
 };
 
